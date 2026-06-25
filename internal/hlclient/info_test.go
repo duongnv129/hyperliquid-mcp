224dc4a52b26ecdb0f000f81221288c90a17d298
Duong Nguyen <duongnv129@gmail.com>
2026-06-25 23:51:46 +0000

    fix: correct JSON test data escaping in info_test.go
    Raw string literals in Go don't process escape sequences, so " was
    being written literally to HTTP responses. Replaced all " with

--- internal/hlclient/info_test.go
+++ internal/hlclient/info_test.go
@@ -47,7 +47,7 @@ func TestUserStateOpenOrders(t *testing.T) {
 		case "clearinghouseState":
 			_, _ = w.Write([]byte(`{"assetPositions": [], "crossMarginSummary": {"accountValue":"100","totalMarginUsed":"0","totalNtlPos":"0","totalRawUsd":"100"}, "marginSummary": {"accountValue":"100","totalMarginUsed":"0","totalNtlPos":"0","totalRawUsd":"100"}, "withdrawable": "100"}`))
 		case "openOrders":
-			_, _ = w.Write([]byte(`[{\"coin\":\"BTC\",\"limitPx\":\"65000\",\"oid\":1,\"origSz\":\"0.1\",\"side\":\"B\",\"sz\":\"0.1\",\"timestamp\":1700000000000}]`))
+			_, _ = w.Write([]byte(`[{"coin":"BTC","limitPx":"65000","oid":1,"origSz":"0.1","side":"B","sz":"0.1","timestamp":1700000000000}]`))
 		default:
 			t.Fatalf("unexpected request type %q", body["type"])
 		}
@@ -74,11 +74,11 @@ func TestFundingHistoryCandlesRecentTrades(t *testing.T) {
 		w.Header().Set("Content-Type", "application/json")
 		switch body["type"] {
 		case "fundingHistory":
-			_, _ = w.Write([]byte(`[{\"coin\":\"BTC\",\"fundingRate\":\"0.0001\",\"premium\":\"0.0002\",\"time\":1700000000000}]`))
+			_, _ = w.Write([]byte(`[{"coin":"BTC","fundingRate":"0.0001","premium":"0.0002","time":1700000000000}]`))
 		case "candleSnapshot":
-			_, _ = w.Write([]byte(`[{\"t\":1700000000000,\"T\":1700000060000,\"i\":\"1m\",\"n\":10,\"o\":\"65000\",\"h\":\"65010\",\"l\":\"64990\",\"c\":\"65005\",\"s\":\"BTC\",\"v\":\"12.5\"}]`))
+			_, _ = w.Write([]byte(`[{"t":1700000000000,"T":1700000060000,"i":"1m","n":10,"o":"65000","h":"65010","l":"64990","c":"65005","s":"BTC","v":"12.5"}]`))
 		case "recentTrades":
-			_, _ = w.Write([]byte(`[{\"coin\":\"BTC\",\"side\":\"B\",\"px\":\"65000\",\"sz\":\"0.1\",\"time\":1700000000000,\"hash\":\"0xabc\",\"tid\":1,\"users\":[\"0x1\",\"0x2\"]}]`))
+			_, _ = w.Write([]byte(`[{"coin":"BTC","side":"B","px":"65000","sz":"0.1","time":1700000000000,"hash":"0xabc","tid":1,"users":["0x1","0x2"]}]`))
 		default:
 			t.Fatalf("unexpected request type %q", body["type"])
 		}
@@ -110,11 +110,11 @@ func TestUserFillsUserFundingOrderStatus(t *testing.T) {
 		w.Header().Set("Content-Type", "application/json")
 		switch body["type"] {
 		case "userFills":
-			_, _ = w.Write([]byte(`[{\"closedPnl\":\"10\",\"coin\":\"BTC\",\"crossed\":true,\"dir\":\"Open Long\",\"hash\":\"0xabc\",\"oid\":1,\"px\":\"65000\",\"side\":\"B\",\"startPosition\":\"0\",\"sz\":\"0.1\",\"time\":1700000000000,\"fee\":\"0.1\",\"feeToken\":\"USDC\",\"tid\":1}]`))
+			_, _ = w.Write([]byte(`[{"closedPnl":"10","coin":"BTC","crossed":true,"dir":"Open Long","hash":"0xabc","oid":1,"px":"65000","side":"B","startPosition":"0","sz":"0.1","time":1700000000000,"fee":"0.1","feeToken":"USDC","tid":1}]`))
 		case "userFunding":
-			_, _ = w.Write([]byte(`[{\"delta\":{\"coin\":\"BTC\",\"fundingRate\":\"0.0001\",\"size\":\"0.1\",\"type\":\"funding\",\"usdc\":\"0.01\"},\"hash\":\"0xabc\",\"time\":1700000000000}]`))
+			_, _ = w.Write([]byte(`[{"delta":{"coin":"BTC","fundingRate":"0.0001","size":"0.1","type":"funding","usdc":"0.01"},"hash":"0xabc","time":1700000000000}]`))
 		case "orderStatus":
-			_, _ = w.Write([]byte(`{\"status\":\"order\",\"order\":{}}`))
+			_, _ = w.Write([]byte(`{"status":"order","order":{}}`))
 		default:
 			t.Fatalf("unexpected request type %q", body["type"])
 		}
